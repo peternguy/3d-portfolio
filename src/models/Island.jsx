83f5e4cadfa1fe0378dac998e86c4425618aa25f
peternguy <pjflipstudio@gmail.com>
2024-12-31 13:18:19 -0600

    trying to deploy

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -12,9 +12,59 @@ import { useFrame, useThree } from '@react-three/fiber'
 import islandScene from '../assets/3d/island.glb'
 import { a } from '@react-spring/three'
 
-const Island = (props) => {
+const Island = ({isRotating, setIsRotating, ...props}) => {
   const { nodes, materials } = useGLTF(islandScene)
   const islandRef = useRef();
+
+  const {gl, viewport} = useThree();
+  const lastX = useRef(0);
+  const rotationSpeed = useRef(0);
+  const dampingFactor = 0.95;
+
+  const handlePointerDown = (e) => {
+    e.stopPropgation();
+    e.preventDefault();
+    setIsRotating(true);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+    lastX.current = clientX;
+  }
+  const handlePointerUp = (e) => {
+    e.stopPropgation();
+    e.preventDefault();
+    setIsRotating(false);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+    const delta = (clientX - lastX.current) / viewport.width;
+
+    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+    lastX.current = clientX;
+    rotationSpeed.current = delta * 0.01 * Math.PI;
+  }
+  const handlePointerMove = (e) => {
+    e.stopPropgation();
+    e.preventDefault();
+
+    if (isRotating) {
+      handlePointerUp(e);
+    }
+  }
+
+  
+
+
+  useEffect(() => {
+    document.addEventListener('pointerdown', handlePointerDown);
+    document.addEventListener('pointerup', handlePointerUp);
+    document.addEventListener('pointermove', handlePointerMove);
+
+    return() => {
+      document.removeEventListener('pointerdown', handlePointerDown);
+      document.removeEventListener('pointerup', handlePointerUp);
+      document.removeEventListener('pointermove', handlePointerMove);
+    }
+  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove])
+
   return (
     <a.group ref={islandRef} {...props}>
       <mesh
