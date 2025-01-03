7375a124d6469c91482158908eb2ed144322ef27
peternguy <pjflipstudio@gmail.com>
2025-01-03 17:11:50 -0600

    popups

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { arrow } from '../assets/icons'
 
 const InfoBox = ({text, link, btnText}) => (
     <div className='info-box'>
-        {text}
-        <Link to={link}>
+        <p className="font-medium sm:text-xl text-center">{text}</p>
+        <Link to={link} className="neo-brutalism-white neo-btn">
             {btnText}
+            <img src={arrow} className="w-4 h-4 object-contain" />
         </Link>
     </div>
 )
@@ -13,19 +15,31 @@ const InfoBox = ({text, link, btnText}) => (
 const renderContent = {
     1: (
         <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-            Hi, I am <span className='font-semibold'> Peter</span>
+            Hi, I'm <span className='font-semibold'> Peter</span>
             <br/>
-            A computer science student in Chicago
+            A computer science student in Chicago 
         </h1>
     ),
     2: (
-        <InfoBox />
+        <InfoBox
+            text = "Always looking to streamline and optimize"
+            link = "/about"
+            btnText = "Learn More" 
+        />
     ),
     3: (
-        <h1>3</h1>
+        <InfoBox
+            text = "Many projects over the years. Curious to see what I've done?"
+            link = "/projects"
+            btnText = "Visit portfolio" 
+        />    
     ),    
     4: (
-        <h1>4</h1>
+        <InfoBox
+            text = "Looking for a dev? Get in touch"
+            link = "/contact"
+            btnText = "Let's talk" 
+        />    
     ),
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Home from './home';
+import Home from './Home';
 import About from './about';
 import Projects from './Projects';
 import Contact from './Contact';
