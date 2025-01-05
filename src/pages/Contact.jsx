1a467e42dbd73a10ac8c25886da507c159a7f497
peternguy <pjflipstudio@gmail.com>
2025-01-05 15:34:17 -0600

    contact + emailjs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,55 @@
 import React, {useState} from 'react'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({name: '', email: '', message: ''})
 
   const [isLoading, setIsLoading] = useState(false);
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value })
+  };
   const handleFocus = () => {};
   const handleBlur = () => {};
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Peter",
+        from_email: form.email,
+        to_email: 'therealpnguyen@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+      //TODO: show success message
+      //TODO: hide an alert
+       
+      setForm({name: '', email:'', message: ''})
+    }).catch((error) => {
+      setIsLoading(false);
+      console.log(error);
+      //TODO: show error message
+    })
+  };
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container ">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <hi className="head-text">Get in Touch</hi>
 
-        <form className="w-full flex flex-col gap-7 mt-14">
+        <form className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
+
           <label className="text-black-500 font-semibold">
             Name
             <input
